58c12f8e43fa3cf56af7b808439f46ee65dd31fc
uchtal <kerim.r.19@gmail.com>
2025-05-13 01:27:05 +0600

    Add files via upload

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,3 +3,75 @@ document.querySelector('.toggleMenu').addEventListener('click', () => {
   document.querySelectorAll(".toggleMenu, .navigation").forEach(elem => elem.classList.toggle("active"));
 })
 // End Navigation
+
+// Shop Slider
+var swiper = new Swiper(".shop .mySwiper", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  loop: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+// End Shop Slider
+
+// GallerySlider
+var swiper = new Swiper(".gallery-slider .mySwiper", {
+  loop: true,
+  spaceBetween: 20,
+  slidesPerView: 1,
+  autoplay: {
+    delay: 3500,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+    },
+    1200: {
+      slidesPerView: 4,
+    },
+  },
+});
+var swiper2 = new Swiper(".gallery-slider .mySwiper2", {
+  loop: true,
+  spaceBetween: 20,
+  slidesPerView: 1,
+  centeredSlides: true,
+  autoplay: {
+    delay: 4500,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 2.01,
+    },
+    1200: {
+      slidesPerView: 4.01,
+    },
+  },
+});
+// End Gallery Slider
+
+// play video on click
+const video_play_btn = document.querySelectorAll(".video-thumbnail");
+video_play_btn.forEach((btn) => {
+  btn.addEventListener("click", function () {
+    this.classList.add("d-none");
+    const videoplayercurrent = this.previousElementSibling;
+    videoplayercurrent.play();
+    videoplayercurrent.controls = true;
+
+    // when video ends remove controls and show video thumbnails
+    videoplayercurrent.addEventListener("ended", function () {
+      videoplayercurrent.controls = false;
+      this.nextElementsSibling.classList.remove("d-none");
+    });
+  });
+});
+// End play video on click
